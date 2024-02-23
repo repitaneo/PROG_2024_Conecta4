1043bb01ebfe4a5f208f908c13924ac08dca8c0a
Marcos <Marcos@Profesor>
2024-02-23 09:48:20 +0100

    Start funcional y Conecta4 con los elementos necesarios

--- src/conecta4/Conecta4.java
+++ src/conecta4/Conecta4.java
@@ -2,4 +2,54 @@ package conecta4;
 
 public class Conecta4 {
 
+	public static final int FILAS = 6; 
+	public static final int COLUMNAS = 7; 
+	
+	private char panel[][];
+	private int turno;
+	
+	public Conecta4() {
+		
+		turno = 1;
+		panel = new char[FILAS][COLUMNAS];
+		
+		for(int i=0;i<FILAS;i++) {
+			for(int j=0;j<COLUMNAS;j++) {
+				
+				panel[i][j] = ' ';
+			}
+		}
+	}
+	
+	
+	
+	public boolean colocarFicha(int columna) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	
+	
+	
+	public void imprimirPanel() {
+		
+		System.out.print("\n");
+
+		for(int i=0;i<FILAS;i++) {
+			for(int j=0;j<COLUMNAS;j++) {
+				
+				System.out.print(panel[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+		for(int j=0;j<COLUMNAS;j++) {
+			
+			System.out.print(j+" ");
+		}
+		
+		System.out.println("\n");
+	}
+
 }
--- src/conecta4/Start.java
+++ src/conecta4/Start.java
@@ -1,10 +1,35 @@
 package conecta4;
 
+import java.util.Scanner;
+
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner teclado = new Scanner(System.in);
+		Conecta4 conecta4 = new Conecta4();
+		
+		boolean hayGanador = false;
+		int turno = 1;
+		
+		while(!hayGanador) {
+			
+			System.out.println("le toca a ["+turno+"]");
+			System.out.println("elige columna:");
+			int columna = teclado.nextInt();
+			hayGanador = conecta4.colocarFicha(columna);
+			conecta4.imprimirPanel();
+			
+			// turno = (turno==1)?2:1;
+			if(turno==1) {
+				turno=2;
+			}
+			else turno = 1;
+		}
+		
 	}
 
+	
+	
 }
